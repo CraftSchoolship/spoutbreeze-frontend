3cfb20124d235dfc96fbdfe01953efd3b05d5d2f
Rayen Dhmaied <dhmaied69@gmail.com>
2025-12-08 01:41:19 +0100

    chore: disable linting for certain files and update app metadata

--- components/settings/integrations/TwitchIntegrationCard.tsx
+++ components/settings/integrations/TwitchIntegrationCard.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import React, { useEffect, useState, useMemo } from "react";
 import {
   Card,
--- components/settings/integrations/YouTubeIntegrationCard.tsx
+++ components/settings/integrations/YouTubeIntegrationCard.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import React, { useEffect, useMemo, useState } from "react";
 import {
   Card,
